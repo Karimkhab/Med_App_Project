d7b6b27bff3bff3bad7f865b943df5add15d2d92
Karimkhab <164759389+Karimkhab@users.noreply.github.com>
2025-06-15 17:07:27 +0300

    New commit

--- Backend Med App/medapp/asgi.py
+++ Backend Med App/medapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for medapp project.
+ASGI config for Backend Med App project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend Med App.settings")
 
 application = get_asgi_application()
--- medapp/medapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from medapp.users.views import telegram_auth_view
-
-urlpatterns = [
-    path("auth/telegram/", telegram_auth_view, name="telegram_auth"),
-]
